4ddb8e11e159aef419b82e83d4d9851ac6e873bf
Francesco Angelo Brisa <fbrisa@gmail.com>
2023-09-18 14:49:41 +0200

    float 64

--- src/field.rs
+++ src/field.rs
@@ -111,6 +111,74 @@ impl Field for Float {
     }
 }
 
+#[derive(Debug)]
+pub struct Double {
+    data_block: i32,
+    /// offset example 8.1
+    /// left side is index within the block
+    /// right side is the bit position only used for bool, zero for all other types
+    offset: f32,
+    value: f64,
+}
+
+impl Double {
+    pub fn new(data_block: i32, offset: f32, mut bytes: Vec<u8>) -> Result<Double, Error> {
+        let len = bytes.len();
+        if bytes.len() != Float::size() as usize {
+            return Err(Error::TryFrom(
+                bytes,
+                format!("Float.new: expected buf size {} got {}", Float::size(), len),
+            ));
+        }
+
+        let bit_offset = ((offset * 10.0) as usize % 10) as u8;
+        if bit_offset != 0 {
+            return Err(Error::TryFrom(
+                bytes,
+                format!(
+                    "Float.new: float should not have a bit offset got {}",
+                    bit_offset
+                ),
+            ));
+        }
+
+        Ok(Double {
+            data_block,
+            offset,
+            value: BigEndian::read_f64(bytes.as_mut_slice()),
+        })
+    }
+
+    pub fn size() -> i32 {
+        8
+    }
+
+    pub fn value(&self) -> f64 {
+        self.value
+    }
+
+    pub fn set_value(&mut self, v: f64) {
+        self.value = v
+    }
+}
+
+impl Field for Double {
+    fn data_block(&self) -> i32 {
+        self.data_block
+    }
+
+    fn offset(&self) -> i32 {
+        self.offset as i32
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buf = vec![0u8; Double::size() as usize];
+        BigEndian::write_f64(buf.as_mut_slice(), self.value);
+        return buf;
+    }
+}
+
+
 /// Bool represents a single bit in a byte from `Area::DataBausteine`
 #[derive(Debug)]
 pub struct Bool {
